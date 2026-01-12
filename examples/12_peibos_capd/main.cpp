0550a315f87f6c1aaa73b985c02a2c02677ae816
godardma <mael.godard@ensta-bretagne.org>
2026-01-12 14:14:11 +0100

    [peibos-capd] adding 3d example

--- examples/12_peibos_capd/main.cpp
+++ examples/12_peibos_capd/main.cpp
@@ -7,6 +7,8 @@ using namespace codac2;
 int main()
 {
   ColorMap cmap = ColorMap::rainbow();
+  VectorVar X_2d(1);
+  VectorVar X_3d(2);
 
   // Julien's example
 
@@ -15,7 +17,6 @@ int main()
   double tf_2d = 8.0;
   double dt_2d = 2.0;
   
-  VectorVar X_2d(1);
   AnalyticFunction psi0_2d ({X_2d},{0.5*X_2d[0],0.5});
 
   OctaSym id_2d ({1,2});
@@ -25,7 +26,7 @@ int main()
 
   Figure2D output ("julien_3_4",GraphicOutput::VIBES);
   output.set_axes(axis(0,{-1,10}),axis(1,{-1,3}));
-  output.set_window_properties({50,100},{800,800});
+  output.set_window_properties({25,100},{800,800});
 
 
   auto m_v_par_2d = reach_set(peibos_output);
@@ -50,12 +51,39 @@ int main()
 
   Figure2D output_pend ("Pendulum",GraphicOutput::VIBES | GraphicOutput::IPE);
   output_pend.set_axes(axis(0,{-2,2}),axis(1,{-4,4}));
-  output_pend.set_window_properties({50,100},{800,800});
+  output_pend.set_window_properties({800,100},{800,800});
 
   auto m_v_par_2d_pend = reach_set(peibos_output_pend);
 
   for (auto& [t, v_par_2d] : m_v_par_2d_pend)
     for (const auto& par: v_par_2d)
       output_pend.draw_parallelepiped(par, cmap.color(t/tf_pend));
+
+  // Lorenz
+
+  Figure3D figure_lorenz ("Lorenz");
+  ColorMap cmap_lorenz = ColorMap::rainbow(0.5);
+
+  capd::IMap vectorField_lorenz("par:sigma,rho,beta;var:x1,x2,x3;fun:10*(x2-x1),28*x1-x2-x1*x3,-2.6*x3+x1*x2;");
+  vectorField_lorenz.setParameter("sigma", 10.);
+  vectorField_lorenz.setParameter("rho", 28.);
+  vectorField_lorenz.setParameter("beta", 8/3);
+
+  double tf_lorenz = 0.1;
+  double dt_lorenz = 0.05;
+
+  AnalyticFunction psi0_lorenz ({X_3d},{1/sqrt(1+sqr(X_3d[0])+sqr(X_3d[1])),X_3d[0]/sqrt(1+sqr(X_3d[0])+sqr(X_3d[1])),X_3d[1]/sqrt(1+sqr(X_3d[0])+sqr(X_3d[1]))});
+
+  OctaSym id_3d ({1,2,3});
+  OctaSym s1 ({-2,1,3});
+  OctaSym s2 ({3,2,-1});
+
+  auto peibos_output_lorenz = PEIBOS(vectorField_lorenz, tf_lorenz, dt_lorenz, psi0_lorenz, {id_3d,s1,s1*s1,s1.invert(),s2,s2.invert()}, 0.1, true);
+
+  auto m_v_par_lorenz = reach_set(peibos_output_lorenz);
+
+  for (auto& [t, v_par_lorenz] : m_v_par_lorenz)
+    for (const auto& par: v_par_lorenz)
+      figure_lorenz.draw_parallelepiped(par, {cmap_lorenz.color(t/tf_lorenz),"t:"+to_string(t)});
 
 }
