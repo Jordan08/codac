51f40350e8df3e7b2e1131da7532e84ed44e77e2
Jordan08 <jordan.ninin@gmail.com>
2026-09-19 02:43:38 +0200

    Set the rounding upward again when oo is initialized, as Codac's own double computations expect

--- src/core/tools/codac2_math.h
+++ src/core/tools/codac2_math.h
@@ -9,13 +9,24 @@
 
 #pragma once
 
-#include <limits>
+#include <gaol/gaol_interval.h>
 #include <numbers>
 #include <cmath>
 
 namespace codac2
 {
-  const double oo = std::numeric_limits<double>::infinity();
+  const double oo = []() {
+
+    // (from IBEX lib, main author: Gilles Chabert)
+    // We use Gaol not in PRESERVE_ROUNDING mode, thus
+    // assuming the rounding mode is always set upward.
+    // Calling this function in the initialization of
+    // the 'oo' constant should be enough as this constant
+    // is initialized before the first Codac function call occurs.
+    gaol::round_upward();
+
+    return std::numeric_limits<double>::infinity();
+  }();
 
   constexpr double PI = std::numbers::pi; // Need C++20
 
